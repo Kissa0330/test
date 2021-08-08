34e820c0ef650912ec59c1dfa7ddfbfd99301f46
yunakash <nakashi1000@gmail.com>
2021-08-08 16:07:58 +0900

    a

--- rush00.c
+++ rush00.c
@@ -13,18 +13,18 @@ char g_w6 = '|';
 
 int rush(int x, int y)
 {
-	if ( x < 1 || y < 1 )
+	if (x < 1 || y < 1)
 	{
 		return (0);
 	}
 	ft_putchar(g_w1);
 	hor_side(x);
-	if (x>1)
+	if (x > 1)
 	{
 		ft_putchar(g_w3);
 	}
 	ft_putchar(g_w5);
-	if (x==1)
+	if (x == 1)
 	{
 		ver_side0(y);
 	}
@@ -69,7 +69,7 @@ void ver_side(int x, int y)
 	}
 }
 
-void ver_side0 (int y)
+void ver_side0(int y)
 {
 	g_i = 2;
 	while (g_i < y)
